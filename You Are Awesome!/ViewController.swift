7fdb97d2fe985e59e53aceeff577fedad8c242a6
James Monahan <Monahajm@bc.edu>
2020-08-31 23:11:53 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by James Monahan on 8/31/20.
+//  Copyright © 2020 James Monahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
